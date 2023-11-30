bacb8cd3618694b166b8b5832243cb66c9e36c91
Bhavesh K <bhavesh.k@softnweb.in>
2023-11-30 13:10:06 +0530

    display home screen properly

--- src/screens/Launch.js
+++ src/screens/Launch.js
@@ -1,16 +1,18 @@
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { colors, images, styles } from '../constants';
 import { GradientButton } from '../components';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Launch() {
+  const Navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Image source={images.Group} resizeMode='stretch'/>
       <Text style={{color:colors.white,fontSize:39}}>Make Payments Much Easier in <Text style={{color:colors.firebase, fontWeight:700}}>one place</Text> </Text>
       <Text style={{color:colors.white,fontSize:19, paddingVertical:15  }}>Your money is safe & growing with us.</Text>
       <View style={{flexDirection:'row',  alignItems:'center', justifyContent:'space-between', paddingTop:20}}>
-        <GradientButton onPress={()=> console.log("test")} title="Get Started" style={styles.btn1} />
-        <TouchableOpacity>
+        <GradientButton onPress={()=> Navigation.navigate("Home")} title="Get Started" style={styles.btn1} />
+        <TouchableOpacity onPress={()=> Navigation.navigate("Home")}>
             <Text style={styles.buttonText}>{'Skip >>'}</Text>
         </TouchableOpacity>
       </View>
